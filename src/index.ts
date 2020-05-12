e0c7bb3c15381a2eb334ee067d7c0163c85c1b9f
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-12 18:55:33 +0100

    Ignore lib/ file for eslint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Used for exporting all parts of the library we want to make public for consumption
-export {QuantiumTesting, StringDefinition} from './QuantiumTester/QuantiumTester';
-export {Range} from './QuantiumTester/range';
-export {RangeError} from './QuantiumTester/errors/range-error';
+export { QuantiumTesting, StringDefinition } from './QuantiumTester/QuantiumTester';
+export { Range } from './QuantiumTester/range';
+export { RangeError } from './QuantiumTester/errors/range-error';
